13da08360a5afb1f19180fea3bdfe38c5d246e14
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2023-07-28 11:00:16 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     <>
 
       <Navigation />
-
+      <HomePage />
       <Routes>
-        <Route path='/' element={<HomePage />} />
+
         <Route path='/favourites' element={<FavouritesPage />} />
       </Routes>
 
